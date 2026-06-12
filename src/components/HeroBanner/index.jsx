944e7b30eaedf1e7554304dd932e8b6bda7b9602
Line Svensen <144799112+LineSvensen@users.noreply.github.com>
2026-06-12 15:22:47 +0200

    5 sek video herobanner

--- src/components/HeroBanner/index.jsx
+++ src/components/HeroBanner/index.jsx
@@ -1,5 +1,5 @@
 import imgOne from "../../assets/bryt-1.webp";
-import vidOne from "../../assets/test-hero.webm";
+import vidOne from "../../assets/PILOT3.mp4";
 
 export default function Herobanner() {
   return (
@@ -13,10 +13,10 @@ export default function Herobanner() {
         preload="metadata"
         poster={imgOne}
       >
-        <source src={vidOne} type="video/webm" />
+        <source src={vidOne} type="video/mp4" />
       </video>
 
-      <div className="absolute inset-0 bg-black/40" />
+      <div className="absolute inset-0 " />
 
       <div className="relative z-10   p-16 text-white">
         <h1 className="text-8xl font-bold">Brytningstid</h1>
