a8a33587c7b58dd913783cf1c132da23f5aac27b
Line Svensen <144799112+LineSvensen@users.noreply.github.com>
2026-06-29 19:03:49 +0200

    playing around. design etc. parallax

--- src/components/SectionThree/index.jsx
+++ src/components/SectionThree/index.jsx
@@ -1,14 +1,18 @@
-
 import bg3 from "../../assets/bg-03.webp";
 
 export default function SecThree() {
   return (
-     <section className="w-full">
-      <img
-        src={bg3}
-        alt=""
-        className="w-full h-auto block"
-      />
-    </section>
+    <>
+      <section
+        className="min-h-screen bg-fixed bg-center bg-cover flex items-center justify-center"
+        style={{ backgroundImage: `url(${bg3})` }}
+      >
+        <h1 className="text-white text-5xl font-bold">Overskrift 3</h1>
+      </section>
+
+      <section className="min-h-screen bg-black text-white flex items-center justify-center">
+        <p className="max-w-2xl text-xl">Innhold 3</p>
+      </section>
+    </>
   );
 }
--- src/components/SectionTwo/index.jsx
+++ src/components/SectionTwo/index.jsx
@@ -2,8 +2,17 @@ import bg2 from "../../assets/bluebg.webp";
 
 export default function SecTwo() {
   return (
-    <section className="w-full">
-      <img src={bg2} alt="" className="w-full h-auto block" />
-    </section>
+    <>
+      <section
+        className="min-h-screen bg-fixed bg-center bg-cover flex items-center justify-center"
+        style={{ backgroundImage: `url(${bg2})` }}
+      >
+        <h1 className="text-white text-5xl font-bold">Overskrift 2</h1>
+      </section>
+
+      <section className="min-h-screen bg-black text-white flex items-center justify-center">
+        <p className="max-w-2xl text-xl">Innhold 2.</p>
+      </section>
+    </>
   );
 }
