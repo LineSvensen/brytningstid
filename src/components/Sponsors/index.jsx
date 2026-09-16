a8745b271dd71d4b7f5e5b33f0bb1cbef431259c
Line Svensen <144799112+LineSvensen@users.noreply.github.com>
2026-09-16 18:27:41 +0200

    changed section two to shadow after clients wish. fixing parallax for phone scroll

--- src/components/Sponsors/index.jsx
+++ src/components/Sponsors/index.jsx
@@ -2,8 +2,8 @@ import dnfBilde from "../../assets/dnfbilde.png";
 
 export function SponsSec() {
   return (
-    <div className="bg-olive-50 flex justify-center">
-      <img src={dnfBilde} className="w-75"></img>
+    <div className="bg-olive-50 flex justify-center ">
+      <img src={dnfBilde} className="w-75 "></img>
     </div>
   );
 }
